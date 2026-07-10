dbd5daacc65b851e955faf1047450bec3912b8eb
quangnh1090-collab <quangnh1090@gmail.com>
2026-07-10 14:06:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker: cài đặt như app & chạy offline
-const CACHE = "thongke-ns-v3";
+const CACHE = "thongke-ns-v4";
 const SHELL = [
   "./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js",
